29acca5f8c0b60bd6ebbe7c38c7c3c36068bb711
Chathurika <chathurika-jayakody.com>
2022-10-30 21:15:52 -0400

    Update home page styles

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -7,10 +7,14 @@ export default function HomePage(){
         <div>
             <img className='hero'src={welcomeImage}/>
             <main className='language'>
-                <Link to='/language/english'><p className='language__name'>English</p></Link>  
-                <Link to='/language/french'><p className='language__name'>French</p></Link> 
-                <Link to='/language/spanish'><p className='language__name'>Spanish</p></Link> 
-                <Link to='/language/chinese'><p className='language__name'>Chinese</p></Link>
+                <h1 className='language__title'>Select a language</h1>
+                <div className='language__list'>
+                    <Link to='/language/english'><p className='language__list-name'>English</p></Link>  
+                    <Link to='/language/french'><p className='language__list-name'>French</p></Link> 
+                    <Link to='/language/spanish'><p className='language__list-name'>Spanish</p></Link> 
+                    <Link to='/language/chinese'><p className='language__list-name'>Chinese</p></Link>
+                </div>
+                
             </main>
         </div>
         
