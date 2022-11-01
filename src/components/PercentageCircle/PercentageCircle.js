e8582823afd6d8d7331ae3a99a80920373016fbc
Chathurika <chathurika-jayakody.com>
2022-10-31 22:13:26 -0400

    Add percentage circle

--- src/components/PercentageCircle/PercentageCircle.js
+++ src/components/PercentageCircle/PercentageCircle.js
@@ -1,7 +1,20 @@
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 import './PercentageCircle.scss'
 
-export default function PercentageCircle(){
+export default function PercentageCircle({amount}){
+    const percentage = amount;
+
     return(
-      <></>
+      <div className='percentage-circle'>
+        <CircularProgressbar value={percentage} text={`${percentage}%`} strokeWidth={13} 
+        styles={buildStyles({
+          textColor: "black",
+          pathColor: "#2EAEB7",
+          trailColor: "#F5622C",
+          strokeLinecap: "butt",
+          textSize: "25px"
+        })}/>;
+      </div>
     )
 }
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -5,6 +5,7 @@ import {Link} from 'react-router-dom';
 import Footer from '../../components/Footer/Footer';
 
 
+
 export default function HomePage(){
     return(
         <div>
